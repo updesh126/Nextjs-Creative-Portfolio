bd85fa1ba7787c9ba88d640e0a6777b01491664f
UpdeshVR <updesh071@gmail.com>
2024-09-18 17:08:50 +0530

    add model and component

---
+++ src/components/RenderModel.jsx
@@ -0,0 +1,18 @@
+"use client"
+import { Environment } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import clsx from 'clsx'
+import React, { Suspense } from 'react'
+
+export const RenderModel = ({children, className}) => {
+  return (
+    <Canvas
+    className={clsx("w-screen h-screen z-10 relative", className)}
+    >
+        <Suspense fallback={null}>
+            {children}
+        </Suspense>
+        <Environment preset='dawn'/>
+    </Canvas>
+  )
+}
